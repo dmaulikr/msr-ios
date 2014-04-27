90741d65649b2fb2a4c37743815bb42b1828dfbe
Ryan P. Schumacher <rschum03@lab105m.eecs.tufts.edu>
2014-04-27 16:42:55 -0400

    intro sequence

--- Player2.m
+++ Player2.m
@@ -15,11 +15,11 @@
 
 /* Delete this comment */
 
--(id)initWorld: (CCPhysicsNode *)_physicsWorld andScene:(GameScene *)_scene{
+-(id)initWorld: (CCPhysicsNode *)_physicsWorld withPosition:(CGPoint)_position andScene:(GameScene *)_scene{
     self = [super init];
     
     _sprite = [CCSprite spriteWithImageNamed:@"martianSmall.png"];
-    _sprite.position  = ccp(_scene.contentSize.width/2,4*_scene.contentSize.height/5);
+    _sprite.position  = _position;
     _sprite.physicsBody = [CCPhysicsBody bodyWithRect:(CGRect){CGPointZero, _sprite.contentSize} cornerRadius:0]; // 1
     _sprite.physicsBody.collisionGroup = @"playerGroup"; // 2
     _sprite.physicsBody.collisionType  = @"playerCollision";
