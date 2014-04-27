bc789492b67cfb658834d6fa8928c27b986168fc
Ryan P. Schumacher <rschum03@lab105m.eecs.tufts.edu>
2014-04-27 14:26:48 -0400

    committing...

--- msr-ios/Classes/IntroScene.m
+++ msr-ios/Classes/IntroScene.m
@@ -45,19 +45,13 @@
     label.position = ccp(0.5f, 0.5f); // Middle of screen
     [self addChild:label];
     
-    // Helloworld scene button
-    CCButton *helloWorldButton = [CCButton buttonWithTitle:@"[ Play ]" fontName:@"Verdana-Bold" fontSize:18.0f];
-    helloWorldButton.positionType = CCPositionTypeNormalized;
-    helloWorldButton.position = ccp(0.4f, 0.35f);
-    [helloWorldButton setTarget:self selector:@selector(onSpinningClicked:)];
-    [self addChild:helloWorldButton];
-
-    // How-to-play button
-    CCButton *howToPlay = [CCButton buttonWithTitle:@"Controls" fontName:@"Verdana-Bold" fontSize:18.0f];
-    howToPlay.positionType   = CCPositionTypeNormalized;
-    howToPlay.position = ccp (0.6, .35f);
-    [self addChild: howToPlay];
-    
+    // Begin-game button
+    CCButton *playGame = [CCButton buttonWithTitle:@"[ Play ]" fontName:@"Verdana-Bold" fontSize:18.0f];
+    playGame .positionType = CCPositionTypeNormalized;
+    playGame.position = ccp(0.4f, 0.35f);
+    [playGame setTarget:self selector:@selector(onSpinningClicked:)];
+    [self addChild:playGame];
+  
     // done
 	return self;
 }
