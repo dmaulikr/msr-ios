4b9caf1616aba1719a5d5f216455ea764a31a33b
macgill <macgill4444@gmail.com>
2014-04-28 17:50:50 -0400

    commented out wind cause it sucked

--- msr-ios/Classes/GameScene.m
+++ msr-ios/Classes/GameScene.m
@@ -36,7 +36,7 @@ bool inIntroScene = true;
     Player *_martian;
     Missile *_missile;
     Powerup *_powerup;
-    Wind *_wind;
+    /*Wind *_wind;*/
     CCLayoutBox *endMenu;
     NSUserDefaults *_defaults;
     int _score;
@@ -235,7 +235,7 @@ bool inIntroScene = true;
     [self schedule:@selector(addMissile:) interval:2];
     [self schedule:@selector(incrementScore) interval:0.1];
     [self schedule:@selector(addPowerup:) interval:4.5];
-    [self schedule:@selector(addWind:) interval:5];
+    /*[self schedule:@selector(addWind:) interval:5];*/
     // In pre-v3, touch enable and scheduleUpdate was called here
     // In v3, touch is enabled by setting userInterActionEnabled for the individual nodes
     // Per frame update is automatically enabled, if update is overridden
@@ -597,15 +597,15 @@ bool inIntroScene = true;
 {
     _powerup = [[Powerup alloc] initWithPhysicsWorld: _physicsWorld andGameScene:self];
 }
-// -----------------------------------------------------------------------
-#pragma mark - Add Powerup
+/*// -----------------------------------------------------------------------
+#pragma mark - Add Wind
 // -----------------------------------------------------------------------
 -(void)addWind:(CCTime)delta
 {
     //random type
     int _type = arc4random() % 3;
     _wind = [[Wind alloc] initWorld:_physicsWorld andScene:self andType:_type];
-}
+}*/
 
 // -----------------------------------------------------------------------
 #pragma mark - Collision Detection for Powerups and player
@@ -649,7 +649,7 @@ bool inIntroScene = true;
     
     return YES;
 }
-// -----------------------------------------------------------------------
+/*// -----------------------------------------------------------------------
 #pragma mark - Collision Detection for Wind and player
 // -----------------------------------------------------------------------
 - (BOOL)ccPhysicsCollisionBegin:(CCPhysicsCollisionPair *)pair windCollision:(CCNode *)wind playerCollision:(CCNode *)player {
@@ -662,7 +662,7 @@ bool inIntroScene = true;
 - (BOOL)ccPhysicsCollisionBegin:(CCPhysicsCollisionPair *)pair windCollision:(CCNode *)wind powerupCollision:(CCNode *)player {
     
     return YES;
-}
+}*/
 
 
 @end
