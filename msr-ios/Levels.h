dfc933dd164fff415905ee28acca5b555070c553
Soubhik Barari <soubhikbarari@gmail.com>
2014-05-03 20:12:38 -0400

    added sounds + continued level interface

--- msr-ios/Levels.h
+++ msr-ios/Levels.h
@@ -6,8 +6,27 @@
 //  Copyright (c) 2014 o2bt. All rights reserved.
 //
 
+#import "cocos2d.h"
+#import "cocos2d-ui.h"
 #import <Foundation/Foundation.h>
 
 @interface Levels : NSObject
+/*
+ cur_background 1
+ cur_background 2
+ cur_background 3
+ asset_dict: backgrounds, clouds, missiles
+ level_length array
+ loop_counter
+ constants
+ 
+ */
+
+-(id)init:(CCScene*)game;
+-(void)addCloud:(CCTime)dt;
+-(void)moveBackground:(CCTime)delta;
+
+
+
 
 @end
